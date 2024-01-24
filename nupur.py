ba8730cca59d192cd9e96de0f6dd30a5a40b38a7
Nupur <2020bcs047@sggs.ac.in>
2024-01-24 22:30:33 +0530

    Reverted Mum Papa

--- nupur.py
+++ nupur.py
@@ -1,10 +1,8 @@
+
+
 ans = input("Whats your name:")
 
 if ans == "Mumma":
     print("She is my mumma")
-    print("Her name is Ragini")
-     
 else:
     print("He is my pappa")
-     print("His name is Manish")
-    
