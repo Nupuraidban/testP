b5fdcac39657fbd534f4c0f268c654b09ae689ea
Nupur <2020bcs047@sggs.ac.in>
2024-01-24 23:01:13 +0530

    done

--- nupur.py
+++ nupur.py
@@ -1,8 +1,6 @@
 ans = input("Whats your name:")
 
 if ans == "Mumma":
-    print("She is my mumma")
     print("She is Nupur's Mumma")
 else:
-    print("He is my pappa")
     print("She is Nupur's Pappa")
